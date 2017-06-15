2231690e057a2b9019a315d707e885cc0d476a4b
Erica Windisch <erica@iopipe.com>
2017-06-15 19:38:49 +0000

    Initial commit
    Signed-off-by: Erica Windisch <erica@iopipe.com>

---
+++ handler.js
@@ -0,0 +1,37 @@
+'use strict';
+const http = require('http')
+const url = require('url')
+
+if (process.env.AWS_SIGN_REQUESTS) {
+  const signRequest = require('aws4').sign
+} else {
+  const signRequest = (requestOptions) => { return requestOptions }
+}
+
+const httpAgent = http.Agent({
+  keepAlive: true,
+  keepAliveMsecs: 300000
+})
+
+module.exports.proxy = (event, context, callback) => {
+  var req = http.request(signRequest({
+    hostname: process.env.URL,
+    method: event.httpMethod,
+    headers: event.headers,
+    path: event.path,
+    agent: httpAgent,
+    timeout: 30 /* maximum time for api gateway invoked Lambda */
+  }))
+
+  var resBody = ""
+  res.on('data', (chunk) => {
+    resBody += chunk
+  })
+  .on('end', () => {
+    callback(null, {
+      statusCode: res.statusCode,
+      headers: res.headers,
+      body: resBody
+    })
+  })
+};
